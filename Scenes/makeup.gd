966afb086b23548d60fcdc56cbeda8509191fb70
Per Kristian Coskun <47058816+Zartok89@users.noreply.github.com>
2026-01-31 15:19:07 +0100

    Updated with "beautify mask" and new textures

---
+++ Scenes/makeup.gd
@@ -0,0 +1,38 @@
+extends Area2D
+
+var is_held := false
+var offset := Vector2.ZERO
+
+const TARGET_GROUP := "face"
+
+var nice_face: Node2D
+
+func _ready():
+	set_process(true)
+	nice_face = get_node("/root/Phase3_Dressup/Character/Peasant1Nice") as Node2D
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MouseButton.MOUSE_BUTTON_LEFT and event.pressed:
+			if not is_held:
+				is_held = true
+				offset = get_global_mouse_position() - global_position
+			else:
+				is_held = false
+
+func _process(delta: float) -> void:
+	if is_held:
+		global_position = get_global_mouse_position() - offset
+
+func _on_area_entered(area):
+	if area.is_in_group(TARGET_GROUP):
+		if area.visible:
+			area.visible = false
+		_trigger_next_function()
+
+func _trigger_next_function():
+	if nice_face:
+		nice_face.visible = true
+		
+	self.visible = false
+	is_held = false
